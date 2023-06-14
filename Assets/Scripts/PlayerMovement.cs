06f584ec7073ae31a716ab29ca9611da95cdcec6
ClarenceLiu5461 <frank1213450@gmail.com>
2023-06-15 07:44:16 +0800

    Player and camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float Speed;
+    public float SidewaysSpeed;
+
+    //We marked this as "Fixed"Update because we are using it to mess with physics
+    void FixedUpdate()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, Speed * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(SidewaysSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SidewaysSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
